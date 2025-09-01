ded201b75c33566fe0cf05f57bb74e084afd1afc
JJ <pattarasaya.sr@kkumail.com>
2025-09-01 22:35:31 +0700

    edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,12 @@ export function Footer() {
         <div className="text-center md:text-left gradient-text ">
           <h2 className="text-lg font-semibold">Contact Me</h2>
           <p className="text-sm mt-2">Email: jj.pattarasaya@gmail.com</p>
-          <p className="text-sm">Phone: 099-209-5588</p>
           <p className="text-sm">Khon Kaen 40000, Thailand</p>
 
         </div>
 
         <div className="flex space-x-4 ">
-        <a href="https://github.com/PattarasayaJJ" target="_blank" rel="noopener noreferrer">
+        <a href="#" target="_blank" rel="noopener noreferrer">
         <img src="https://github.githubassets.com/favicons/favicon.png" alt="GitHub" />
 
   </a>
